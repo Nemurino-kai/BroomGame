44378bd87ad086568104e490c705d8b9fa28d22a
DoubleZ <mirenai1997@yahoo.co.jp>
2019-03-16 14:56:24 +0900

    さまざまな障害物をテクスチャ付きで出せるようにした

--- gameManager.cpp
+++ gameManager.cpp
@@ -8,8 +8,8 @@ gameManager::gameManager():world(Vec2(0, -2.0)),camera(Vec2(1, 2.5), 120)
 	wheel = world.createWheelJoint(wall, broom, broom.getPos() + Vec2(0.04, 0), Vec2(0, 1), WheelJointState(false, 20.0, 60, 0.5));
 	previousPos = Window::GetState().windowPos;
 	previousDelta = Vec2(0, 0);
-	
 	timer.start();
+	obstacleManage = std::make_unique<obstacleManager>(this);
 }
 
 bool gameManager::isOver()
@@ -21,7 +21,7 @@ void gameManager::update()
 {
 	if (Input::MouseL.clicked)
 	{
-		balls.push_back(world.createCircle(Vec2(0, 4), 0.1));
+		balls.push_back(std::make_pair(world.createCircle(Vec2(0, 4), 0.1),L"baseball"));
 	}
 
 	Vec2 currentPos = Window::GetState().windowPos;
@@ -40,13 +40,12 @@ void gameManager::update()
 	}
 	if (Input::KeyQ.clicked) {
 		//balls.push_back(world.createCircle(Vec2(-4, 3), 0.1));
-		balls.push_back(world.createCircle(Vec2(-4,3),0.24,PhysicsMaterial(0.3)));
-		balls[int(balls.size()) - 1].applyLinearImpulse(Vec2(3.0, 0)*balls[int(balls.size()) - 1].getMass());
+		addObstacle(Vec2(-4, 3), 0.24, PhysicsMaterial(0.3), L"soccer");
+		balls[int(balls.size()) - 1].first.applyLinearImpulse(Vec2(3.0, 0)*balls[int(balls.size()) - 1].first.getMass());
 	}
 	if (Input::KeyW.clicked) {
-		//balls.push_back(world.createCircle(Vec2(4, 3), 0.1));
-		balls.push_back(world.createCircle(Vec2(4, 3), 0.3, PhysicsMaterial(0.3)));
-		balls[int(balls.size()) - 1].applyLinearImpulse(Vec2(-3.0, 0)*balls[int(balls.size()) - 1].getMass());
+		addObstacle(Vec2(4, 3), 0.3, PhysicsMaterial(0.3), L"basket");
+		balls[int(balls.size()) - 1].first.applyLinearImpulse(Vec2(-3.0, 0)*balls[int(balls.size()) - 1].first.getMass());
 	}
 
 	previousPos = currentPos;
@@ -76,9 +75,9 @@ void gameManager::draw()
 
 	for (const auto& ball : balls)
 	{
-		ball.draw(HSV(0.5, 0.4, 1.0));
-		Texture(TextureAsset(L"basket")).scale(1.0 / camera.getScale()).drawAt(ball.getPos());
-		//Texture(TextureAsset(L"soccer")).scale(1.0 / camera.getScale()).drawAt(ball.getPos());
+		ball.first.draw(HSV(0.5, 0.4, 1.0));
+
+		Texture(TextureAsset(ball.second)).scale(1.0 / camera.getScale()).drawAt(ball.first.getPos());
 		Println(camera.getScale());
 	}
 
@@ -92,4 +91,40 @@ void gameManager::draw()
 double gameManager::getTime()
 {
 	return 	timer.ms()/10/100.0;
+}
+
+void gameManager::addObstacle(Vec2 vec, double size,PhysicsMaterial material, String name)
+{
+	balls.push_back(std::make_pair(world.createCircle(vec, size, material), name));
+}
+
+obstacleManager::obstacleManager(gameManager *setGame)
+{
+	gameManage = setGame;
+	previousTime = 0.0;
+}
+
+void obstacleManager::update()
+{
+	double nowTime = gameManage->getTime();
+	//1•b–ˆ‚É”»’è
+	if (nowTime - previousTime>1.0) {
+		//áŠQ•¨‚ªo‚é‚©—””»’è
+		if (Random() < 0.3) {
+			//‚Ç‚ÌáŠQ•¨‚ğo‚·‚©Œˆ’è
+			switch (Random(1, 3))
+			{
+			case 1:
+				//gameManage->balls.push_back(std::make_pair(world.createCircle(Vec2(0, 4), 0.1), L"baseball"));
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			default:
+				break;
+			}
+
+		}
+	}
 }
--- gameManager.h
+++ gameManager.h
@@ -2,18 +2,20 @@
 #include <Siv3d.hpp>
 # include <HamFramework.hpp>
 
+class obstacleManager;
 
 class gameManager {
 private:
 	bool endflag = false;
 
 	Stopwatch timer;
+	std::unique_ptr<obstacleManager> obstacleManage;
 	//std::unique_ptr<PhysicsWorld> world;
 
 	PhysicsWorld world;
 	PhysicsBody wall;
 	PhysicsBody broom;
-	Array<PhysicsBody> balls;
+	Array<std::pair<PhysicsBody,String>> balls;
 	PhysicsWheelJoint wheel;
 	CameraBox2D camera;
 	Vec2 previousPos;
@@ -25,4 +27,16 @@ public:
 	void update();
 	void draw();
 	double getTime();
+	void addObstacle(Vec2 vec,double size,PhysicsMaterial material, String name);
+};
+
+//1秒毎に現在のタイムに応じて、障害物を投げるかを決める。
+class obstacleManager {
+	gameManager* gameManage;
+	//前回障害物を投げたタイム
+	double previousTime;
+
+public:
+	obstacleManager(gameManager* setGame);
+	void update();
 };
